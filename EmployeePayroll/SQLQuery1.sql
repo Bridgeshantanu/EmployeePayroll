a33e00fdac044cb60b3eb838ff72cf6dd9faefcb
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-27 17:46:52 +0530

    [Shantanu]: retrieve data code is added

--- EmployeePayroll/SQLQuery1.sql
+++ EmployeePayroll/SQLQuery1.sql
@@ -5,3 +5,5 @@ create table employee_payroll(id int identity(1,1) primary key,name varchar(25),
 insert into employee_payroll values('ram',20000,'2023-05-04'),
 ('raj',25000,'2022-08-10'),
 ('shyam',25000,'2020-08-18');
+
+select * from employee_payroll;
