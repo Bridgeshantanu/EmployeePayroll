2a5c4ecf00207e2076f60859045cdebb3c98a47c
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-27 17:43:02 +0530

    [Shantanu]: insert data code is added

--- EmployeePayroll/SQLQuery1.sql
+++ EmployeePayroll/SQLQuery1.sql
@@ -1,3 +1,7 @@
 create database payroll_service
 
 create table employee_payroll(id int identity(1,1) primary key,name varchar(25),Salary float,StartDate date);
+
+insert into employee_payroll values('ram',20000,'2023-05-04'),
+('raj',25000,'2022-08-10'),
+('shyam',25000,'2020-08-18');
