3d7ea0bf34497393f9945155a7d144e1a363ff64
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-27 18:48:08 +0530

    [Shantanu]: extend table code is added

--- EmployeePayroll/SQLQuery1.sql
+++ EmployeePayroll/SQLQuery1.sql
@@ -22,3 +22,9 @@ select min(salary) as minimum_salary from employee_payroll where gender = 'M' gr
 select avg(salary) as average_salary from employee_payroll where gender = 'M' group by gender;
 select count(salary) as number_of_employees from employee_payroll where gender = 'M' group by gender;
 select count(salary) as number_of_employees from employee_payroll where gender = 'F' group by gender;
+
+select * from employee_payroll;
+alter table employee_payroll add phonenumber bigint, address varchar(50), department varchar(50);
+update employee_payroll set phonenumber = 705418, address = 'xyz', department = 'manager' where id=1;
+update employee_payroll set phonenumber = 452552, address = 'abc', department = 'supervisor' where id=2;
+update employee_payroll set phonenumber = 705785, address = 'pqr', department = 'maintenance' where id=3;
